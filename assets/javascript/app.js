55e8b3c493a20fe44d05f4062d6a4b81bff08c1f
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-05-19 09:39:12 -0400

    issues with firebase update; changed firebase scripts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,52 @@
-var gameData = new Firebase("https://rps-multiplayer-5636.firebaseio.com/");
-
-var playerData = gameData.child("players");
-
-playerData.remove();
-
-$("#enterName").on("click", function(){
-	var playerName = $("#playerName").val().trim();
-	$("#waiting1").empty();
-	$("#player1").html(playerName);
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyA3lWq9MJGHa-r5oiFdgFlrhwFpF-JrTzM",
+  //   authDomain: "rps-multiplayer-5636-39611.firebaseapp.com",
+  //   databaseURL: "https://rps-multiplayer-5636-39611.firebaseio.com",
+  //   storageBucket: "rps-multiplayer-5636-39611.appspot.com",
+  // };
+  // firebase.initializeApp(config);
 
 
 
-	playerData.child("1").set({
-		name: playerName,
-		wins: 0,
-		losses: 0
-	})
-	return false;
-});
+//   var gameData = firebase.database();
+
+// console.log(gameData);
+
+// function writeUserData(name, email) {
+//   gameData.ref('players/').set({
+//     username: Kirby,
+//     email: email
+//   });
+// }
+
+
+
+// var playerData = gameData.child("players");
+
+// if(playerData.child("1")){
+// 	playerNumber = "2";
+// }
+// else if(playerData.child("2")){
+// 	playerNumber = "1";
+// }
+
+// console.log(playerData.child("1"));
+// console.log(playerNumber);
+
+
+// playerData.remove();
+
+// $("#enterName").on("click", function(){
+// 	var player1Name = $("#playerName").val().trim();
+// 	$("#waiting1").empty();
+// 	$("#player1").html(player1Name);
+
+// 	// gameData.ref("players").set({
+// 	// 	name: playerName,
+// 	// 	wins: 0,
+// 	// 	losses: 0
+// 	// })
+// 	return false;
+// });
 
