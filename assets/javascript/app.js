56d8d5e3e52a0a22d160ac6649ff4e465ccd01a6
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-05-19 11:26:22 -0400

    wanting to remove only child node on refresh

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ firebase.initializeApp(config);
 
 var gameData = firebase.database();
 
-gameData.ref("players/1").remove();
+gameData.ref("players").child("1").remove();
 
 // console.log(gameData);
 
